cd0f632ecdf7ddf7665e8e588b4de8606b62a213
Bryan Cauê Vaz <bcaue177@gmail.com>
2026-04-09 16:56:06 -0300

    login finished

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ from flask import Flask, render_template, request, redirect, url_for
 # request -> pega dados do formulário
 # redirect -> redireciona para outra rota
 # url_for -> gera URL de uma rota
-from flask_login import LoginManager, login_user, login_required
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from db import db
 from models import Usuario
+import hashlib # para criptografar coisas
 
 # app vai ser o representante do Flask no código
 app = Flask(__name__)
@@ -18,8 +19,13 @@ app.secret_key = 'LongaMarcha10' # Aqui o melhor é usar um dotenv para esconder
 lm = LoginManager(app) # aqui o app serve de parametro para o LoginManager gerenciar
 # aqui o banco de dados sqlite será criado e o config procurará o arquivo database.db
 # se ele não existir, ele automaticamente o criará
+lm.login_view = 'login' # Aqui diz que se o usuário não estivar logado, ele redireciona para login
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 db.init_app(app) # conectando banco ao Flask
+
+def hash(txt):
+    hash_obj = hashlib.sha256(txt.encode('utf-8'))
+    return hash_obj.hexdigest()
 
 @lm.user_loader # carrega usuário logado pelo id
 def user_loader(id):
@@ -31,7 +37,7 @@ def user_loader(id):
 def home():
     return render_template('home.html')
 
-@app.route('/login', method=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
         return render_template('login.html')
@@ -39,9 +45,12 @@ def login():
         nome = request.form['nomeForm']
         senha = request.form['senhaForm']
 
-        user = db.session.query(Usuario).filter_by(nome=nome, senha=senha).first()
+        user = db.session.query(Usuario).filter_by(nome=nome, senha=hash(senha)).first()
         if not user:
             return 'Nome ou senha incorretos'
+
+        login_user(user)
+        return redirect(url_for('home'))
 
 @app.route('/registrar', methods=['GET', 'POST'])
 def registrar():
@@ -59,6 +68,11 @@ def registrar():
 
         return redirect(url_for('home'))
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 # Aqui a gente diz para o python executar isso somente se esse arquivo for o principal
 # O "debug=True" apeans ativa uma funcionalidade de auxílio ao dev caso haja bugs no código.
 # Ao que parece, ao lançar o projeto para o público, o certo é tirar o debug. 
